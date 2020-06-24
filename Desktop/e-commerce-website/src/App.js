0d312384ac5c84f00d15ca81e253c22198741c8d
vivekpandey790 <v.b.pandey@accenture.com>
2020-06-25 04:03:17 +0530

    directory  menu components along with home page

--- Desktop/e-commerce-website/src/App.js
+++ Desktop/e-commerce-website/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HomePage from './components/homepage/home-page.component'
+import HomePage from './pages/homepage/home-page.component'
 import './App.css';
 
 function App() {
