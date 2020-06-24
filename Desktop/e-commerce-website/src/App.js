7e33495df774a1db3c851669489855e49a315561
vivekpandey790 <v.b.pandey@accenture.com>
2020-06-25 02:48:28 +0530

    first version

---
+++ Desktop/e-commerce-website/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomePage from './components/homepage/home-page.component'
+import './App.css';
+
+function App() {
+  return (
+    <HomePage />
+  );
+}
+
+export default App;
